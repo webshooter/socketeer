4c49597b05badb3b6a8927ffb3064d369b547aeb
Randall Nickerson <webshooter@gmail.com>
2021-04-13 10:47:53 -0700

    add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: "airbnb-base",
+  env: {
+    browser: true,
+    node: true,
+    "jest/globals": true,
+  },
+  rules: {
+    quotes: ["error", "double"],
+  },
+};
