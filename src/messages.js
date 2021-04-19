68c3e2e7c0822c616f55f742d50b91427432bee1
Randall Nickerson <webshooter@gmail.com>
2021-04-19 14:57:56 -0700

    add room-closing message to messages

--- src/messages.js
+++ src/messages.js
@@ -1,6 +1,7 @@
 export const keys = {
   SERVER_GREET: "server-greet",
   ROOM_GREET: "room-greet",
+  ROOM_CLOSING: "room-closing",
 };
 
 const messages = new Map();
@@ -14,6 +15,15 @@ messages.set(keys.ROOM_GREET, ({ client, room, error }) => ({
   key: keys.ROOM_GREET,
   id: client.id,
   error,
+  room: {
+    id: room.id,
+    name: room.name,
+    clients: room.clients,
+  },
+}));
+
+messages.set(keys.ROOM_CLOSING, ({ room } = {}) => ({
+  key: keys.ROOM_CLOSING,
   room: {
     id: room.id,
     name: room.name,
